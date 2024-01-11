1da74060b1f9f431f70bd1b280330e6ff75cf807
[philip iacone] <[philiacone@gmail.com]>
2024-01-10 19:21:50 -0500

    correcting controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,16 @@
-const routes = require('express').Router();
-
-routes.get('/', (req, res) => {
-  res.render('home')
-})
-
-module.exports = routes;
+// Importing Express Router module
+const router = require('express').Router();
+// Importing routes for home page
+const homeRoutes = require('./homeRoutes.js');
+// Importing routes for dashboard
+const dashboardRoutes = require('./dashboardRoutes.js');
+// Importing API routes
+const apiRoutes = require('./api');
+// Using the homeRoutes 
+router.use('/', homeRoutes);
+// Using the dashboardRoutes 
+router.use('/dashboard', dashboardRoutes);
+// Using apiRoutes 
+router.use('/api', apiRoutes);
+// Exporting router
+module.exports = router;
