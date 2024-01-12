986754e25febe95cee99eae2cf5498db64b68a76
[philip iacone] <[philiacone@gmail.com]>
2024-01-12 10:19:00 -0500

    Added styling and js

---
+++ public/js/comment.js
@@ -0,0 +1,24 @@
+const commentFormHandler = async function (event) {
+	event.preventDefault();
+	const postId = document.querySelector("input[name='postId']").value;
+	const commentBody = document.querySelector(
+		"textarea[name='comment-body']"
+	).value;
+	if (commentBody) {
+		await fetch('/api/comment', {
+			method: 'POST',
+			body: JSON.stringify({
+				postId,
+				commentBody,
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		document.location.reload();
+	}
+};
+
+document
+	.querySelector('#new-comment-form')
+	.addEventListener('submit', commentFormHandler);
---
+++ public/js/logout.js
@@ -0,0 +1,19 @@
+const logout = async () => {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+const logoutBtn = document.querySelector('#logout');
+if (logoutBtn) {
+  logoutBtn.addEventListener('click', logout);
+}
+});
